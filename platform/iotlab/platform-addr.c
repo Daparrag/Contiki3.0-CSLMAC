549ced95af9c8aed2d6f1b3e5cfeb6552f3f023e
Ana GARCIA ALCALA <anagalcala_17@hotmail.com>
2015-05-06 11:40:51 +0200

    Change rimeaddr_node_addr for linkaddr_node_addr

--- platform/iotlab/platform-addr.c
+++ platform/iotlab/platform-addr.c
@@ -6,32 +6,32 @@ void set_rime_addr()
 {
 #if RIMEADDR_SIZE == 2
     uint16_t short_uid = platform_uid();
-    rimeaddr_node_addr.u8[0] = 0xff & (short_uid >> 8);
-    rimeaddr_node_addr.u8[1] = 0xff & (short_uid);
+    linkaddr_node_addr.u8[0] = 0xff & (short_uid >> 8);
+    linkaddr_node_addr.u8[1] = 0xff & (short_uid);
 #else
 
 #define IOTLAB_UID_ADDR 0
 #if !(IOTLAB_UID_ADDR)
     /* Company 3 Bytes */
-    rimeaddr_node_addr.u8[0] = 0x01;
-    rimeaddr_node_addr.u8[1] = 0x23;
-    rimeaddr_node_addr.u8[2] = 0x45;
+    linkaddr_node_addr.u8[0] = 0x01;
+    linkaddr_node_addr.u8[1] = 0x23;
+    linkaddr_node_addr.u8[2] = 0x45;
 
     /* Platform identifier */
-    rimeaddr_node_addr.u8[3] = 0x01;
+    linkaddr_node_addr.u8[3] = 0x01;
 
     /* Generate 4 remaining bytes using uid of processor */
     // use bytes 8-11 to ensure uniqueness (tested empirically)
     int i;
     for (i = 0; i < 4; i++)
-        rimeaddr_node_addr.u8[i+4] = uid->uid8[i+8];
+        linkaddr_node_addr.u8[i+4] = uid->uid8[i+8];
 
 #else
-    memset(&rimeaddr_node_addr, 0, sizeof(rimeaddr_node_addr));
+    memset(&linkaddr_node_addr, 0, sizeof(linkaddr_node_addr));
     uint16_t short_uid = platform_uid();
-    rimeaddr_node_addr.u8[0] = 0x02;
-    rimeaddr_node_addr.u8[6] = 0xff & (short_uid >> 8);
-    rimeaddr_node_addr.u8[7] = 0xff & (short_uid);
+    linkaddr_node_addr.u8[0] = 0x02;
+    linkaddr_node_addr.u8[6] = 0xff & (short_uid >> 8);
+    linkaddr_node_addr.u8[7] = 0xff & (short_uid);
 
 #endif
 #endif
