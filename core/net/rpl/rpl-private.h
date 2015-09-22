ca943f6c4e2178834ee979d4bd589b8cb092d0c2
Joakim Eriksson <joakime@sics.se>
2015-09-22 23:21:03 +0200

    made ZERO LIFETIME routes live 30 seconds instead of 60 and shortened route lifetime in travis test

--- core/net/rpl/rpl-private.h
+++ core/net/rpl/rpl-private.h
@@ -155,7 +155,7 @@
 
 
 /* Expire DAOs from neighbors that do not respond in this time. (seconds) */
-#define DAO_EXPIRATION_TIMEOUT          60
+#define DAO_EXPIRATION_TIMEOUT          30
 /*---------------------------------------------------------------------------*/
 #define RPL_INSTANCE_LOCAL_FLAG         0x80
 #define RPL_INSTANCE_D_FLAG             0x40
