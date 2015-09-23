d4e7d211abd24e117e98bacf88d9b7a38426234b
Joakim Eriksson <joakime@sics.se>
2015-09-23 09:12:35 +0200

    fixed so that DAO NACK now is either for the path or for RPL root - if from root then do not try to switch parent since that will not help

--- core/net/rpl/rpl-icmp6.c
+++ core/net/rpl/rpl-icmp6.c
@@ -661,6 +661,7 @@ dao_input(void)
   int learned_from;
   rpl_parent_t *parent;
   uip_ds6_nbr_t *nbr;
+  int is_root;
 
   prefixlen = 0;
   parent = NULL;
@@ -693,6 +694,8 @@ dao_input(void)
   sequence = buffer[pos++];
 
   dag = instance->current_dag;
+  is_root = (dag->rank == ROOT_RANK(instance));
+
   /* Is the DAG ID present? */
   if(flags & RPL_DAO_D_FLAG) {
     if(memcmp(&dag->dag_id, &buffer[pos], sizeof(dag->dag_id))) {
@@ -827,7 +830,8 @@ dao_input(void)
     if(flags & RPL_DAO_K_FLAG) {
       /* signal the failure to add the node */
       dao_ack_output(instance, &dao_sender_addr, sequence,
-             RPL_DAO_ACK_UNABLE_TO_ACCEPT);
+		     is_root ? RPL_DAO_ACK_UNABLE_TO_ACCEPT_ROOT :
+		     RPL_DAO_ACK_UNABLE_TO_ACCEPT);
     }
     goto discard;
   }
@@ -836,11 +840,11 @@ dao_input(void)
   if(rep == NULL) {
     RPL_STAT(rpl_stats.mem_overflows++);
     PRINTF("RPL: Could not add a route after receiving a DAO\n");
-
     if(flags & RPL_DAO_K_FLAG) {
       /* signal the failure to add the node */
       dao_ack_output(instance, &dao_sender_addr, sequence,
-             RPL_DAO_ACK_UNABLE_TO_ACCEPT);
+		     is_root ? RPL_DAO_ACK_UNABLE_TO_ACCEPT_ROOT :
+		     RPL_DAO_ACK_UNABLE_TO_ACCEPT);
     }
     goto discard;
   }
