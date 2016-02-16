ecc68ddc58d08e2f62b1d1d76133a24f804c5f22
Yasuyuki Tanaka <yasuyuki.tanaka@inf.ethz.ch>
2016-02-16 15:14:06 +0100

    Remove an unnecessary NULL-check in insert_srh_header()

--- core/net/rpl/rpl-ext-header.c
+++ core/net/rpl/rpl-ext-header.c
@@ -380,7 +380,7 @@ insert_srh_header()
   cmpri = 15;
   cmpre = 15;
 
-  if(node != NULL && node == root_node) {
+  if(node == root_node) {
     PRINTF("RPL: SRH no need to insert SRH\n");
     return 0;
   }
