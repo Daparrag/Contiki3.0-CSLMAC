a955c9c9c6f8a767bc1eb5a7887f4b745e1b30e5
Simon Duquennoy <simon.duquennoy@inria.fr>
2016-03-03 13:57:09 +0100

    TSCH example: unset RF2XX_SOFT_PREPARE

--- examples/ipv6/rpl-tsch/project-conf.h
+++ examples/ipv6/rpl-tsch/project-conf.h
@@ -67,6 +67,9 @@
 #define TSCH_CALLBACK_JOINING_NETWORK tsch_rpl_callback_joining_network
 #define TSCH_CALLBACK_LEAVING_NETWORK tsch_rpl_callback_leaving_network
 
+/* Needed for IoT-LAB M3 nodes */
+#undef RF2XX_SOFT_PREPARE
+#define RF2XX_SOFT_PREPARE 0
 /* Needed for cc2420 platforms only */
 /* Disable DCO calibration (uses timerB) */
 #undef DCOSYNCH_CONF_ENABLED
